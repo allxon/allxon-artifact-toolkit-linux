f038c9ba722c5789a8a74171f05420a4a8f3cf9e
Ethan.Wu <ewu@allxon.com>
2022-10-18 12:25:13 +0800

    Allxon Artifact Toolkit 1.0.0 Release

---
+++ README.md
@@ -0,0 +1,45 @@
+# What is Allxon Artifact Toolkit
+Allxon Artifact Toolkit is a simple yet powerful tool that packages all kinds of files into a unified format to support seamless operations during Allxon OTA deployment across a mass fleet of remote edge AI devices. Allxon Artifact Toolkit not only allows you to intuitively customize your OTA script and process, but it only requires four simple steps to complete artifact packaging. Whether you want to update docker image, firmware, BSP image, application file, software, script, image, video, etc., you can easily package it into an Allxon verified artifact that is readable by Allxon Portal, giving you a smoother user experience. 
+
+# Allxon Artifact Toolkit Structure 
+The Allxon Artifact Toolkit Structure below shows you the path to correctly input your files and directories in order to successfully package your OTA artifact. 
+```
+allxon-artifact-toolkit-linux
+├── ota_content
+│   └── ota_deploy.sh
+├── artifact_tool.sh
+└── README.md
+```
+- **ota_content**:  the folder where you will put all the necessary files you want to deploy on your devices.
+- **ota_deploy.sh**: the executable script that handles all operations related to OTA deployment, allowing you to customize your OTA deployment process to suit your needs. 
+- **artifact_tool.sh**: a shell script that is used to generate and test an OTA artifact. 
+
+
+
+# How to Generate OTA Artifact Using Allxon Artifact Toolkit 
+1. Make sure you have put everything (docker image, firmware, BSP image, application file, software, script, image, video, etc.) you are going to deploy onto your devices into the `ota_content` directory. 
+2. Edit `ota_deploy.sh` to suit your needs. (e.g. calling an executable file to execute an action or run an application update).
+3. Run `$sudo artifact_tool.sh –-package` to generate an OTA artifact. 
+4. Run `$sudo artifact_tool.sh --test {file name of artifact}` to test the Allxon OTA artifact you generated. This action will simulate OTA deployment by running `ota_deploy.sh`, therefore, the artifact will be deployed on the host. 
+5. Verify if the deployment operation is executed as expected as specified in `ota_deploy.sh`.
+
+
+
+# Note
+
+By default, the architecuture of the artifact generated will be the same as the host architecture. (e.g. `x86`, `x86_64` or `aarch64`).
+
+* Specify the architecture for an artifact with `--arch` argument.
+`$sudo artifact_tool.sh --arch aarch64 –-package`
+   * List of supported architecture
+     * `x86`
+     * `x86_64`
+     * `aarch64`
+* Generate an artifact for all the supported architecture with `--arch all` 
+`$sudo artifact_tool.sh --arch all –-package`   
+   
+   
+# What is Next
+Once you have generated your Allxon Artifact Toolkit, head over to [Allxon Portal](https://dms.allxon.com/) to start performing OTA updates from the cloud portal to fleets of edge devices! 
+
+Follow the instrustions to [deploy Allxon OTA artifact](https://www.allxon.com/knowledge/deploy-ota-artifact).
---
+++ artifact_tool.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+set -e
+
+VERSION=1.0.0
+
+WORKDIR=${PWD}
+
+IS_NVIDIA_JETSON=0
+HOST_ARCH=UNKNOW
+DISTRIB_ID=UNKNOW
+TARGET_ARCH=
+
+print_err() {
+    echo -ne "\033[0;31m$1\n"
+}
+
+show_uage() {
+  echo -ne "Usage: --package or --test <path_to_file>\n"
+  echo -ne '\t  [ --arch <ARCH|all>] --package \t create ota package file with <ARCH>. --arch all for all architecture\n'
+  echo -ne '\t  --test path_to_file  \t\t\t test <path_to_file> ota package on local device\n'
+        
+  exit 2
+}
+
+check_host_dist() {
+
+    local dist=`uname -s`
+    echo $dist
+    if [ ! $dist = "Linux" ]; then
+        print_err "This tool is for Linux platform only!!!\n"
+        exit 2
+    fi
+
+    DISTRIB_ID=`awk -F "=" 'NR==1{print $2 ; eixt}' /etc/*-release`
+    
+    if [ -f "/etc/nv_tegra_release" ]; then
+        IS_NVIDIA_JETSON=1
+    fi
+}
+
+check_host_arch() {
+    arch=`uname -m`
+    if [ -n ${arch} ]; then
+        HOST_ARCH=${arch}
+    fi
+}
+
+show_versio_info() {
+    echo -ne "********************************************************************* \n"
+    echo -ne "\t Allxon package tool : ${VERSION}\n"
+    if [ $IS_NVIDIA_JETSON -eq 0 ]; then
+        echo -ne "\t Host : ${DISTRIB_ID}\n";
+    else
+        echo -ne "\t Host : Jetson\n";
+    fi
+
+    echo -ne "\t Architecture : ${HOST_ARCH}\n";
+    
+    TARGET_ARCH=${HOST_ARCH}
+
+	echo -ne "********************************************************************* \n"
+}
+
+create_host_info_at_pwd() {
+    uname -a > .host_info
+    echo "ota_tool_ver=${VERSION}" >>.host_info
+    echo "command=${USER_COMMAND}" >>.host_info
+}
+
+do_package() {
+
+    echo -ne "Creating Allxon OTA Artifact ...\n"
+    
+    echo -ne "Using Architecture : *****[${TARGET_ARCH}]*****\n"
+
+    local PKG_NAME=Allxon_OTA_Artifact-L-${TARGET_ARCH}.tar.gz
+    local NEW_PKG_NAME=
+
+    if [ -f "${PKG_NAME}" ]; then 
+        rm ${PKG_NAME}
+    fi
+
+    cd ota_content
+    
+    create_host_info_at_pwd
+
+    echo -ne "Packing all files in ota_content...\n"
+    tar cvzf ../${PKG_NAME} *
+    
+    cd ${WORKDIR}
+
+    FILE_HASH=`md5sum ${PKG_NAME} | awk '{ print $1 }'`
+    FILE_NAME_HASH=`echo -ne ${FILE_HASH}-${PKG_NAME} | md5sum | awk '{print $1}'`
+
+    NEW_PKG_NAME="${FILE_NAME_HASH}-${PKG_NAME}"
+    mv ${PKG_NAME} ${NEW_PKG_NAME}
+
+    echo -ne "Allxon OTA Artifact Created : ${NEW_PKG_NAME} \n"
+
+    exit 0
+}
+
+do_install() {
+
+    OTA_PAYLOAD_PACKAGE=$1;
+    if [ ! -f "${OTA_PAYLOAD_PACKAGE}" ]; then
+        print_err "$1 not exists. \n"
+        exit 1
+    fi
+    FILENAME=$(basename ${OTA_PAYLOAD_PACKAGE})
+    FILE_HASH=`md5sum ${FILENAME} | awk '{ print $1 }'`
+    local ORI_PKG_NAME=${FILENAME: 33}
+    echo "Origin name : ${ORI_PKG_NAME}"
+
+    FILE_NAME_HASH=`echo -ne ${FILE_HASH}-${ORI_PKG_NAME} | md5sum | awk '{print $1}'`
+    NEW_FILE_NAME=${FILE_NAME_HASH}-${ORI_PKG_NAME}
+    
+    if [ ! "${NEW_FILE_NAME}" = "${FILENAME}" ]; then
+        print_err "Package is not intact\n"
+        exit 1
+    fi
+
+    TARGET_ARCH=${FILENAME: 55: -7}
+
+    if  [ ${TARGET_ARCH} != "all" ] && [ ${TARGET_ARCH} != ${HOST_ARCH} ] ; then
+        print_err "Packge is not supported on this machine\n"
+        exit 1
+    fi
+
+    echo -ne "Extarcting Allxon OTA Artifact : ${OTA_PAYLOAD_PACKAGE} \n"
+    
+    DESTDIR=/tmp/${NOW_TS}
+    mkdir -p /tmp/${NOW_TS}
+
+    echo "Extract ${OTA_PAYLOAD_PACKAGE}"
+    if ! tar xzvf "${OTA_PAYLOAD_PACKAGE}" -C "${DESTDIR}" >/dev/null 2>&1; then
+        print_err "Failed to run \"tar xzvf ${OTA_PAYLOAD_PACKAGE} -C ${DESTDIR}\"\n"
+        exit 1
+    fi
+
+    echo -ne "\n\nRuning install.sh in Allxon OTA Artifact ... \n"
+
+    cd ${DESTDIR} && source ./ota_deploy.sh
+
+    cd ${WORKDIR}
+
+    echo "Running install is finished. Check the result."
+    exit 0
+}
+
+set_target_arch() {
+    TARGET_ARCH=$1
+}
+
+echo "$@"
+
+USER_COMMAND=`echo "$0 $*"`
+
+check_host_arch
+check_host_dist
+
+show_versio_info
+
+# Make sure that this script is running in root privilege
+USERID=$(id -u)
+if [ "${USERID}" -ne 0 ]; then
+       print_err "Please run this tool as root.\n"
+       exit 1
+fi
+
+while [ True ]; do
+if [ "$1" = "--package" -o "$1" = "-p" ]; then
+    do_package
+elif [ "$1" = "--test" -o "$1" = "-t" ]; then
+    do_install $2
+elif [ "$1" = "--arch" -o "$1" = "-a" ]; then
+    set_target_arch $2
+    shift 2
+else
+    show_uage
+fi
+    
+done
